4505bed9ef1373907d6ba115f53c5471d2e07806
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-10-30 12:53:40 +0200

    Render meal types

--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
 
+import "./menu.css";
+
 class Menu extends Component {
+  state = {
+    types: [
+      { id: 1, name: "Levesek" },
+      { id: 2, name: "Főételek" },
+      { id: 3, name: "Vegetáriánus" },
+      { id: 4, name: "Frissensültek" },
+      { id: 5, name: "Tészták" },
+      { id: 6, name: "Köretek" },
+      { id: 7, name: "Desszertek" },
+      { id: 8, name: "Saláták" },
+    ],
+  };
+
   render() {
-    return <h1>Menu</h1>;
+    const { types } = this.state;
+
+    return (
+      <section>
+        {types.map(({ id, name }) => (
+          <h1 key={id}>{name}</h1>
+        ))}
+      </section>
+    );
   }
 }
 
