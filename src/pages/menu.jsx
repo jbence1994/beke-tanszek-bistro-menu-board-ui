f1822bec76dd5b403438ef90fd828cbd4f03eae9
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-10-30 14:10:19 +0200

    Move hard-coded mock data to a service component

--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -3,76 +3,16 @@ import React, { Component } from "react";
 import Logo from "../components/logo";
 import MenuBoard from "../components/menuBoard";
 
+import getTypes from "../services/typeService";
+
 class Menu extends Component {
   state = {
-    types: [
-      {
-        id: 1,
-        name: "Levesek",
-        meals: [
-          { id: 1, name: "Meal #1" },
-          { id: 2, name: "Meal #2" },
-        ],
-      },
-
-      {
-        id: 2,
-        name: "Főételek",
-        meals: [
-          { id: 1, name: "Meal #1" },
-          { id: 2, name: "Meal #2" },
-        ],
-      },
-      {
-        id: 3,
-        name: "Vegetáriánus",
-        meals: [
-          { id: 1, name: "Meal #1" },
-          { id: 2, name: "Meal #2" },
-        ],
-      },
-      {
-        id: 4,
-        name: "Frissensültek",
-        meals: [
-          { id: 1, name: "Meal #1" },
-          { id: 2, name: "Meal #2" },
-        ],
-      },
-      {
-        id: 5,
-        name: "Tészták",
-        meals: [
-          { id: 1, name: "Meal #1" },
-          { id: 2, name: "Meal #2" },
-        ],
-      },
-      {
-        id: 6,
-        name: "Köretek",
-        meals: [
-          { id: 1, name: "Meal #1" },
-          { id: 2, name: "Meal #2" },
-        ],
-      },
-      {
-        id: 7,
-        name: "Desszertek",
-        meals: [
-          { id: 1, name: "Meal #1" },
-          { id: 2, name: "Meal #2" },
-        ],
-      },
-      {
-        id: 8,
-        name: "Saláták",
-        meals: [
-          { id: 1, name: "Meal #1" },
-          { id: 2, name: "Meal #2" },
-        ],
-      },
-    ],
+    types: [],
   };
+
+  componentDidMount() {
+    this.setState({ types: getTypes() });
+  }
 
   render() {
     const { types } = this.state;
