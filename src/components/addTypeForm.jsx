0901a18c795677ccfe5d7ac666a1e3a967721637
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-10-30 16:55:15 +0200

    Add type form

---
+++ src/components/addTypeForm.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class AddTypeForm extends Component {
+  state = {};
+
+  render() {
+    return <h1>AddTypeForm</h1>;
+  }
+}
+
+export default AddTypeForm;
