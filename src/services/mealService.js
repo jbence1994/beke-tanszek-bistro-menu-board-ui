3c40e48b087e95d637a8706e94b376a34c1f3428
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-10-31 10:06:35 +0100

    Create a service to fetch all meals

--- src/services/mealService.js
+++ src/services/mealService.js
@@ -2,6 +2,11 @@ import restClient from "axios";
 
 import endpoints from "./endpoints.json";
 
+const getMeals = async () => {
+  const endpoint = endpoints.getMeals;
+  return await restClient.get(endpoint);
+};
+
 const createMeal = async (meal) => {
   const endpoint = endpoints.createMeal;
   return await restClient.post(endpoint, meal);
