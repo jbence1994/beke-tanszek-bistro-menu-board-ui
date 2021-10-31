1aedc0872aad5833a05558bac674ad32a19cc5c5
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-10-31 11:49:13 +0100

    Implement daily menu creating

--- src/components/dailyMenuForm.jsx
+++ src/components/dailyMenuForm.jsx
@@ -1,10 +1,56 @@
 import React, { Component } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import MealsDropdown from "./mealsDropdown";
+import Submit from "./common/submit";
+
+import { createDailyMenu } from "../services/dailyMenusService";
 
 class DailyMenuForm extends Component {
-  state = {};
+  state = {
+    dailyMenu: {
+      mealId: 0,
+    },
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { dailyMenu } = this.state;
+      const { data } = await createDailyMenu(dailyMenu);
+
+      const { meal } = data;
+      const { name: mealName } = meal;
+
+      toast.success(`${mealName} sikeresen hozzáadva a napi menühöz!`, {
+        position: "top-left",
+      });
+    } catch (e) {
+      toast.error("Étel hozzáadása a napi menühöz sikertelen!", {
+        position: "top-left",
+      });
+    }
+  };
+
+  handleNumberChange = ({ currentTarget: input }) => {
+    const dailyMenu = { ...this.state.dailyMenu };
+    dailyMenu[input.name] = parseInt(input.value);
+    this.setState({ dailyMenu });
+  };
 
   render() {
-    return <h1>Daily Menu Form</h1>;
+    return (
+      <React.Fragment>
+        <h2>Napi menü összeállítása</h2>
+        <form noValidate>
+          <MealsDropdown onChange={this.handleNumberChange} />
+          <Submit text="Mentés" onSubmit={this.handleSubmit} />
+        </form>
+        <ToastContainer autoClose={3000} />
+      </React.Fragment>
+    );
   }
 }
 
