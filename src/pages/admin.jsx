d64bc1f16afd52a633af7a1a3f80445b3efe61bb
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-10-31 11:43:23 +0100

    Insert Daily Menu Form to admin page

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import AddTypeForm from "../components/addTypeForm";
 import AddMealForm from "../components/addMealForm";
+import DailyMenuForm from "../components/dailyMenuForm";
 
 class Admin extends Component {
   state = {
@@ -15,6 +16,9 @@ class Admin extends Component {
       <div className="container">
         <h1 className="p-3 text-center">{pageName}</h1>
         <div className="row">
+          <div className="col-12 mt-5">
+            <DailyMenuForm />
+          </div>
           <div className="col-12 mt-5">
             <AddTypeForm />
           </div>
