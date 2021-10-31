6264935260ebd628c7cf2b87d1f21189df05c1be
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-10-31 09:44:13 +0100

    Create component's base

---
+++ src/components/dailyMenuForm.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class DailyMenuForm extends Component {
+  state = {};
+
+  render() {
+    return <h1>Daily Menu Form</h1>;
+  }
+}
+
+export default DailyMenuForm;
