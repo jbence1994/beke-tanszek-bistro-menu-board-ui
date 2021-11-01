ef638e957de3a9a301e0d72e9effc1a9f064caad
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-11-01 14:29:11 +0100

    Restyle contact component

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,26 +1,14 @@
 import React, { Component } from "react";
 
 class Contact extends Component {
-  state = {
-    companyInfo: {
-      name: "Béke Tanszék Vendéglátó Bt.",
-      address: "6722 Szeged, Boldogasszony sugárút 11.",
-      phone: "+36-70/778-7150",
-      fax: "+36-62/424-177",
-    },
-  };
-
   render() {
-    const { companyInfo } = this.state;
-    const { name, address, phone, fax } = companyInfo;
-
     return (
-      <div className="mt-3 text-center">
-        <h3>Üzemeltető:</h3>
-        <h3>{name}</h3>
-        <h3>{address}</h3>
-        <h3>Tel.: {phone}</h3>
-        <h3>Fax: {fax}</h3>
+      <div className="text-center">
+        <h5>Üzemeltető:</h5>
+        <h5>Béke Tanszék Vendéglátó Bt.</h5>
+        <h5>6722 Szeged, Boldogasszony sugárút 11.</h5>
+        <h5>Tel.: +36-70/778-7150</h5>
+        <h5>Fax: +36-62/424-177</h5>
       </div>
     );
   }
