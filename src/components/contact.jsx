13c061bcdba84f2724f2f7cf6aa4302ddd317ab8
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-11-01 15:29:10 +0100

    Increase font size

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,12 +3,12 @@ import React, { Component } from "react";
 class Contact extends Component {
   render() {
     return (
-      <div className="text-center">
-        <h5>Üzemeltető:</h5>
-        <h5>Béke Tanszék Vendéglátó Bt.</h5>
-        <h5>6722 Szeged, Boldogasszony sugárút 11.</h5>
-        <h5>Tel.: +36-70/778-7150</h5>
-        <h5>Fax: +36-62/424-177</h5>
+      <div className="text-center mb-5">
+        <h4>Üzemeltető:</h4>
+        <h4>Béke Tanszék Vendéglátó Bt.</h4>
+        <h4>6722 Szeged, Boldogasszony sugárút 11.</h4>
+        <h4>Tel.: +36-70/778-7150</h4>
+        <h4>Fax: +36-62/424-177</h4>
       </div>
     );
   }
