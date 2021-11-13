fb06e661215598e400865a25f1b02bd73f48e360
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-11-13 14:12:21 +0100

    Rename parameter

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -15,8 +15,8 @@ class Contact extends Component {
 
     return (
       <div className="text-center mb-5">
-        {info.map((row) => (
-          <h3>{row}</h3>
+        {info.map((element) => (
+          <h3>{element}</h3>
         ))}
       </div>
     );
