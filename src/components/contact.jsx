58959dcbe2ef60eaf45d0a6725d4d7081f6e3882
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-11-13 14:22:30 +0100

    Enhancement to contact

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,10 +3,9 @@ import React, { Component } from "react";
 class Contact extends Component {
   state = {
     info: [
-      "Üzemeltető:",
       "Béke Tanszék Vendéglátó Bt.",
       "6722 Szeged, Boldogasszony sugárút 11.",
-      "Tel.: +36 70 389 21 23",
+      "+36 70 389 21 23",
     ],
   };
 
