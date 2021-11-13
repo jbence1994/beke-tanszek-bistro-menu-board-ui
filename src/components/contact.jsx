e26d41836531a530c20c5633f0630fee67d145bc
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-11-13 14:03:45 +0100

    Dynamically render contact info

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
 
 class Contact extends Component {
+  state = {
+    info: [
+      "Üzemeltető:",
+      "Béke Tanszék Vendéglátó Bt.",
+      "6722 Szeged, Boldogasszony sugárút 11.",
+      "Tel.: +36 70 389 21 23",
+    ],
+  };
+
   render() {
+    const { info } = this.state;
+
     return (
       <div className="text-center mb-5">
-        <h4>Üzemeltető:</h4>
-        <h4>Béke Tanszék Vendéglátó Bt.</h4>
-        <h4>6722 Szeged, Boldogasszony sugárút 11.</h4>
-        <h4>Tel.: +36 70 389 21 23</h4>
+        {info.map((row) => (
+          <h3>{row}</h3>
+        ))}
       </div>
     );
   }
